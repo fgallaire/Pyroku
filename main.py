e39bfa6dfea3db9de12378c5fae789ca73ca2056
Florent Gallaire <fgallaire@gmail.com>
2022-03-13 00:53:43 +0100

    remove comments

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from pyrogram import Client
 
 api_id = int(environ["API_ID"])
 api_hash = environ["API_HASH"]
-bot_token = environ.get("BOT_TOKEN", None) # Optional
-session_string = environ.get("SESSION_STRING", ":memory:") # if variable empty it will pass :memory: instead
+bot_token = environ.get("BOT_TOKEN")
+session_string = environ.get("SESSION_STRING", ":memory:")
 info = "Greetings from **Heroku**!"
 
 app = Client(session_string, api_id, api_hash, bot_token=bot_token)
