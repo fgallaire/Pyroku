438e2975e883b94bd5ac10639887605ca520d19c
Florent Gallaire <fgallaire@gmail.com>
2022-03-12 03:13:10 +0100

    userbot compatible

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from pyrogram import Client
 
 api_id = int(environ["API_ID"])
 api_hash = environ["API_HASH"]
-bot_token = environ["BOT_TOKEN"]
+bot_token = environ.get("BOT_TOKEN")
+session_string = environ.get("SESSION_STRING")
 info = "Greetings from **Heroku**!"
 
-app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
+if bot_token:
+    app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
+elif session_string:
+    app = Client(session_string, api_id, api_hash)
 
 print(info)
 
